92a319ad9f632eb32686dd064ebe3cc2be45a75b
EINDEX <snowstarlbk@gmail.com>
2021-05-06 20:14:38 +0800

    remove background color

--- web/src/components/BlogFooter.vue
+++ web/src/components/BlogFooter.vue
@@ -1,16 +1,17 @@
 <template>
   <footer>
     <div
-      class="bg-gray-300 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-4 items-centers justify-items-center"
+      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-6 pb-4 items-centers justify-items-center"
     >
-      <div class="grid h-24 grid-cols-4 gap-4 justify-items-center my-4">
-        <div class="col-start-4">
+      <div class="grid h-24 md:grid-cols-4 sm:grid-cols-1 gap-4 justify-items-center my-4">
+        <div class="md:col-start-4 sm:col-auto">
           <div class="flex flex-row gap-4">
             <img class="h-24 rounded-2xl" src="/@/assets/avatar.png" />
             <div class="col-start-4">
               <div class="flex flex-col items-start justify-around leading-6 h-full">
                 <a
                   href="https://github.com/eindex"
+                  rel="noreferrer"
                   target="_blank"
                   class="flex align-middle flex-row h-6 gap-1"
                 >
@@ -35,6 +36,7 @@
         <a
           class="flex flex-row h-4 leading-4 gap-1 text-gray-500"
           href="http://www.beian.gov.cn/portal/index"
+          rel="noreferrer"
           target="_blank"
           ><img class="h-4 w-4" src="/@/assets/gongan.png" />鄂ICP备15018232号-1</a
         >
--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <div class="bg-gray-200 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16">
         <div class="flex items-center">
           <div class="flex-shrink-0">
